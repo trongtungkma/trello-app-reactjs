a80f40ce9c9612424665e8a67047bc43f17f6bfb
tunglt <trongtung@mirabo.com.vn>
2021-08-08 23:10:52 +0700

    [feature] build Project and design basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,341 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trello-react-app">
+      <div className="navbar app">navbar app</div>
+      <div className="navbar board">navbar board</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>footer</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+        <div className="column">
+          <header>header</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
+                alt="trogunt"
+              />
+              Title : Trongtung
+            </li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+            <li>content</li>
+          </ul>
+          <footer>add a card</footer>
+        </div>
+      </div>
     </div>
   );
 }
